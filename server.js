c3a102df1ae6f1c42844842fba31463eeedac6dd
root <root@cds4.localdomain>
2023-08-04 21:28:15 +0300

    a

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { injectSocketIO } from './utils/socketIoHandler.js';
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { path: '/socket/' });
+
+// Serve your built project
+app.use(express.static('dist'));
+
+// Inject Socket.IO
+injectSocketIO(io);
+
+// Start the server
+const port = process.env.PORT || 3001;
+httpServer.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
