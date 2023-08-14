ceee8f7ebd1f9e93e5de93b02f714ae65f3440a1
root <root@cds4.localdomain>
2023-08-14 15:49:27 +0300

    a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,12 @@
         </div>
         <div id="middle">
             <div id="menu">
-
+                <div class="menu-container">
+                    <a href="#" class="menu-item">Item 1</a>
+                    <a href="#" class="menu-item">Item 2</a>
+                    <a href="#" class="menu-item">Item 3</a>
+                    <!-- Add more items as needed -->
+                </div>                
             </div>
             <div id="body">
 
@@ -65,4 +70,38 @@
         padding: 20px;
         height: 100%;
     }
+
+    /* Container for the menu */
+    .menu-container {
+        width: 250px; /* Adjust as needed */
+        background-color: #f7f7f7; /* Light gray background */
+        border: 1px solid #e0e0e0; /* Border around the menu */
+        border-radius: 5px; /* Rounded corners */
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); /* Subtle shadow for depth */
+        overflow: hidden; /* Ensures inner items don't overflow the rounded corners */
+    }
+
+    /* Individual menu items */
+    .menu-item {
+        display: block; /* Make each item a block element */
+        padding: 10px 20px; /* Padding for each item */
+        color: #333; /* Text color */
+        text-decoration: none; /* Remove underline for links */
+        font-size: 16px; /* Adjust font size as needed */
+        font-weight: 500; /* Semi-bold font weight */
+        border-bottom: 1px solid #e0e0e0; /* Separator between items */
+        transition: background-color 0.3s; /* Smooth transition for hover effect */
+    }
+
+    /* Hover effect for menu items */
+    .menu-item:hover {
+        background-color: #e0e0e0; /* Change background color on hover */
+        color: #000; /* Darker text color on hover */
+    }
+
+    /* Remove border for the last menu item */
+    .menu-item:last-child {
+        border-bottom: none;
+    }
+
 </style>
