f3fa82007f958ad66b30845415251b90c9c678a8
root <root@cds4.localdomain>
2023-08-16 09:19:35 +0300

    a

---
+++ src/routes/rooms/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Layout from '../../components/Layout.svelte';
+    import InfiniteTable from '../../components/InfiniteTable.svelte';
+</script>
+
+<Layout>
+    <h2>Rooms</h2>
+    
+    <InfiniteTable />
+      
+</Layout>
---
+++ src/routes/specialists/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Layout from '../../components/Layout.svelte';
+    import InfiniteTable from '../../components/InfiniteTable.svelte';
+</script>
+
+<Layout>
+    <h2>Specialists</h2>
+    
+    <InfiniteTable />
+      
+</Layout>
