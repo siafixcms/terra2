9c6599c1ad3e055769dedbf8c458305d52f5e4ee
root <root@cds4.localdomain>
2023-08-16 13:48:19 +0300

    a

---
+++ src/routes/appointments/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Layout from '../../components/Layout.svelte';
+    import InfiniteTable from '../../components/InfiniteTable.svelte';
+</script>
+
+<Layout>
+    <h2>Appointments</h2>
+    
+    <InfiniteTable />
+      
+</Layout>
