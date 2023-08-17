d510e2336920697c3428658cfc90e6d30fe3f1a4
root <root@cds4.localdomain>
2023-08-17 18:37:17 +0300

    a

---
+++ src/components/API.js
@@ -0,0 +1,20 @@
+export async function fetchData(query = '', filters = {}, page = 1) {
+    const params = new URLSearchParams({
+        q: query,
+        ...filters,
+        page,
+        per_page: 30
+    });
+
+    const response = await fetch(`/api?${params.toString()}`);
+    const data = await response.json();
+
+    return data;
+}
+
+export async function fetchTotalRecords() {
+    const response = await fetch('/api/totalRecords');
+    const { total } = await response.json();
+
+    return total;
+}
---
+++ src/components/Accordion.svelte
@@ -0,0 +1,20 @@
+<script>
+    let isOpen = false;
+</script>
+
+<button on:click={() => isOpen = !isOpen}>
+    {isOpen ? 'Close Filters' : 'Open Filters'}
+</button>
+
+<div class="{isOpen ? 'open' : 'closed'}">
+    <slot></slot>
+</div>
+
+<style>
+    .open {
+        display: block;
+    }
+    .closed {
+        display: none;
+    }
+</style>
