495c4f9bf5bf9810f075da72e135ed2258011917
root <root@cds4.localdomain>
2023-08-17 12:01:13 +0300

    a

---
+++ src/routes/employee_availability/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Layout from '../../components/Layout.svelte';
+    import InfiniteTable from '../../components/InfiniteTable.svelte';
+    import Calendar from '@event-calendar/core';
+    import TimeGrid from '@event-calendar/time-grid';
+
+    let plugins = [TimeGrid];
+    let options = {
+        view: 'timeGridWeek',
+        editable: true,
+        events: [
+            // your list of events
+        ]
+    };
+</script>
+
+<Layout>
+    <h2>Employee availability</h2>
+    
+    <Calendar {plugins} {options} />
+      
+</Layout>
+
+<style>
+    
+</style>
