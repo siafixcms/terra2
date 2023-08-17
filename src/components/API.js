02218a3ff3218a45456f38e0a58a6e99782f4a68
root <root@cds4.localdomain>
2023-08-17 19:13:00 +0300

    a

--- src/components/API.js
+++ src/components/API.js
@@ -9,18 +9,18 @@ export async function fetchData(query = '', filters = {}, page = 1) {
     const response = await fetch(`/api?${params.toString()}`);
     const data = await response.json();
 
-    return data ? data : [];
+    return data && data.length ? data : [];
 }
 
 export async function fetchTotalRecords() {
     const response = await fetch('/api/totalRecords');
     const { total } = await response.json();
 
-    return total ? total : [];
+    return total && total.length ? total : [];
 }
 
 export async function fetchDistinctValues(field) {
     const response = await fetch(`/api/distinctValues?field=${field}`);
     const data = await response.json();
-    return data ? data : [];
+    return data && data.length ? data : [];
 }
