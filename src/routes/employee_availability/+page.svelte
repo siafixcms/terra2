2cf676e1e562ad863ed5c6b37a5d9e6dc8eafb38
root <root@cds4.localdomain>
2023-08-23 17:28:57 +0300

    a

--- src/routes/employee_availability/+page.svelte
+++ src/routes/employee_availability/+page.svelte
@@ -1,7 +1,5 @@
 <script>
-    import { onMount } from 'svelte';
     import Layout from '../../components/Layout.svelte';
-    import InfiniteTable from '../../components/InfiniteTable.svelte';
     import Calendar from '@event-calendar/core';
     import TimeGrid from '@event-calendar/time-grid';
 
@@ -9,50 +7,14 @@
     let options = {
         view: 'timeGridWeek',
         editable: true,
-        events: [],
+        events: [
+            
+        ],
         pointer: true,
         select: function(info) {
-            const newEvent = {
-                title: 'New Event',
-                start: info.start,
-                end: info.end,
-                allDay: info.allDay
-            };
-            options.events = [...options.events, newEvent];
+            console.log(info);
         }
     };
-
-    let calendarInstance;
-
-    onMount(() => {
-        calendarInstance = new Calendar({
-            plugins,
-            ...options,
-            events: options.events,
-            eventClick: function(info) {
-                options.events = options.events.filter(event => event !== info.event);
-            }
-        });
-        calendarInstance.renderTo(document.querySelector('#calendar-container'));
-    });
-
-    // function saveEventsToDatabase() {
-    //     // Make API call to save events to the database
-    //     fetch('your-save-events-api-url', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify(options.events)
-    //     })
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         console.log('Events saved successfully:', data);
-    //     })
-    //     .catch(error => {
-    //         console.error('Error saving events:', error);
-    //     });
-    // }
 </script>
 
 <Layout>
