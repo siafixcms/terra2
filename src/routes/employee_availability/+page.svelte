a4356088e12e787936b2efd8cb26cc3733d3816f
root <root@cds4.localdomain>
2023-08-23 16:43:13 +0300

    a

--- src/routes/employee_availability/+page.svelte
+++ src/routes/employee_availability/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { onMount } from 'svelte';
     import Layout from '../../components/Layout.svelte';
     import InfiniteTable from '../../components/InfiniteTable.svelte';
     import Calendar from '@event-calendar/core';
@@ -8,20 +9,56 @@
     let options = {
         view: 'timeGridWeek',
         editable: true,
-        events: [
-            
-        ],
+        events: [],
         pointer: true,
         select: function(info) {
-            console.log(info);
+            const newEvent = {
+                title: 'New Event',
+                start: info.start,
+                end: info.end,
+                allDay: info.allDay
+            };
+            options.events = [...options.events, newEvent];
         }
     };
+
+    let calendarInstance;
+
+    onMount(() => {
+        calendarInstance = new Calendar({
+            plugins,
+            ...options,
+            events: options.events,
+            eventClick: function(info) {
+                options.events = options.events.filter(event => event !== info.event);
+            }
+        });
+        calendarInstance.renderTo(document.querySelector('#calendar-container'));
+    });
+
+    function saveEventsToDatabase() {
+        // Make API call to save events to the database
+        fetch('your-save-events-api-url', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(options.events)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Events saved successfully:', data);
+        })
+        .catch(error => {
+            console.error('Error saving events:', error);
+        });
+    }
 </script>
 
 <Layout>
     <h2>Employee availability</h2>
     
-    <Calendar {plugins} {options} />
+    <div id="calendar-container" />
       
 </Layout>
 
