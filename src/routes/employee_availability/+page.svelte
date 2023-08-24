ee65eafd6fdd1b8a38ab45663225f6008d8645e5
root <root@cds4.localdomain>
2023-08-24 10:26:55 +0300

    a

--- src/routes/employee_availability/+page.svelte
+++ src/routes/employee_availability/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { onMount, onDestroy } from 'svelte';
     import Layout from '../../components/Layout.svelte';
     import Calendar from '@event-calendar/core';
     import TimeGrid from '@event-calendar/time-grid';
@@ -7,20 +8,76 @@
     let options = {
         view: 'timeGridWeek',
         editable: true,
-        events: [
-            {title: "test", start: "2023-08-23 10:00:00", end: "2023-08-23 15:00:00"},
-            {title: "test1", start: "2023-08-22 10:00:00", end: "2023-08-22 12:00:00"},
-            {title: "test2", start: "2023-08-21 08:00:00", end: "2023-08-21 13:00:00"},
-        ],
+        events: [],
         pointer: true,
         select: function(info) {
-            console.log(info);
+            const newEvent = {
+                title: 'New Event',
+                start: info.start,
+                end: info.end,
+                allDay: info.allDay
+            };
+            options.events = [...options.events, newEvent];
         }
     };
+
+    let calendarInstance;
+
+    onMount(() => {
+        calendarInstance = new Calendar({
+            plugins,
+            ...options,
+            events: options.events,
+            eventClick: function(info) {
+                options.events = options.events.filter(event => event !== info.event);
+            }
+        });
+        calendarInstance.renderTo(document.querySelector('#calendar-container'));
+    });
+
+    onDestroy(() => {
+        // Clean up the calendar instance when the component is destroyed
+        calendarInstance.destroy();
+    });
+
+    async function saveEventsToDatabase() {
+        try {
+            // Make API call to save events to the database
+            const response = await fetch('your-save-events-api-url', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(options.events)
+            });
+
+            const data = await response.json();
+            console.log('Events saved successfully:', data);
+        } catch (error) {
+            console.error('Error saving events:', error);
+        }
+    }
+
+    async function loadEventsFromDatabase() {
+        try {
+            // Make API call to retrieve events from the database
+            const response = await fetch('your-get-events-api-url');
+            const data = await response.json();
+
+            // Update events in the options
+            options.events = data;
+            calendarInstance.refetchEvents(options.events);
+        } catch (error) {
+            console.error('Error loading events:', error);
+        }
+    }
 </script>
 
 <Layout>
     <h2>Employee availability</h2>
+    
+    <button on:click={saveEventsToDatabase}>Save Events</button>
+    <button on:click={loadEventsFromDatabase}>Load Events</button>
     
     <Calendar {plugins} {options} />
       
