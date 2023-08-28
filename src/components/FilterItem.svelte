3811548667c4960387e33b2df1207f3159d5aae3
root <root@cds4.localdomain>
2023-08-28 15:10:02 +0300

    a

--- src/components/FilterItem.svelte
+++ src/components/FilterItem.svelte
@@ -1,46 +1,54 @@
 <script>
     import { createEventDispatcher } from 'svelte';
+    import { Dropdown } from '@flowbite/svelte';
     const dispatch = createEventDispatcher();
   
     export let field;
     export let header;
     export let options;
-    export let selectValue;
+    export let selectValue = [];
   
-    function handleChange(event) {
-      const selectedOptions = Array.from(event.target.selectedOptions).map(option => option.value);
-      dispatch('update', { field, selectedOptions });
+    function handleSelect(option) {
+      if (selectValue.includes(option)) {
+        selectValue = selectValue.filter(item => item !== option);
+      } else {
+        selectValue = [...selectValue, option];
+      }
+      dispatch('update', { field, selectedOptions: selectValue });
     }
-</script>
-
-<div class="filter-item">
+  </script>
+  
+  <div class="filter-item">
     <label for="{field}">{header}</label>
-    <select id="{field}" multiple bind:value={selectValue} on:change={handleChange} class="bg-none">
+    <Dropdown let:toggle let:menu>
+      <button use:toggle class="btn btn-outline-primary">
+        {header}
+        <i class="bi bi-caret-down-fill"></i>
+      </button>
+      <div use:menu class="dropdown-menu">
         {#each options as option}
-        <option value={option}>{option}</option>
+          <a class="dropdown-item" on:click={() => handleSelect(option)}>
+            <input type="checkbox" bind:checked={selectValue.includes(option)} />
+            {option}
+          </a>
         {/each}
-    </select>
-</div>
-
-<style>
+      </div>
+    </Dropdown>
+  </div>
+  
+  <style>
     .filter-item {
-        flex-basis: calc(20% - 10px);
-        max-width: 20%;
-        margin-bottom: 20px;
-        box-sizing: border-box;
-        align-self: flex-start;
+      flex-basis: calc(20% - 10px);
+      max-width: 20%;
+      margin-bottom: 20px;
+      box-sizing: border-box;
+      align-self: flex-start;
     }
-
+  
     .filter-item label {
-        display: block;
-        font-weight: bold;
-        margin-bottom: 5px;
+      display: block;
+      font-weight: bold;
+      margin-bottom: 5px;
     }
-
-    .filter-item select {
-        width: 100%;
-        padding: 5px;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-    }
-</style>
+  </style>
+  
