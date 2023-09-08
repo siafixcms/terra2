0ca1613c61a411bfd4e46fd372160a431c8af201
root <root@cds4.localdomain>
2023-09-08 13:43:53 +0300

    a

--- src/routes/clients/+page.svelte
+++ src/routes/clients/+page.svelte
@@ -1,11 +1,15 @@
 <script>
     import Layout from '../../components/Layout.svelte';
     import InfiniteTable from '../../components/InfiniteTable.svelte';
+    import PopupButton from '../../components/PopupButton.svelte';
+    let context = { data: null };
 </script>
 
 <Layout>
 
     <h2 class="text-2xl font-semibold text-gray-800 my-4">Clients</h2>
+
+    <PopupButton {context} />
     
     <InfiniteTable importbaseUrl={'clients'} />
     
