95862ca167d6c9e0f08a180fb686ca473c7ebcc1
root <root@cds4.localdomain>
2023-09-12 13:10:14 +0300

    a

--- src/routes/rooms/+page.svelte
+++ src/routes/rooms/+page.svelte
@@ -5,7 +5,11 @@
 
 <Layout>
     <h2 class="text-2xl font-semibold text-gray-800 my-4">Rooms</h2>
+
+    <div class="flex justify-end mb-4">
+        <PopupButton importbaseUrl={'rooms'} title={'Add a new room'} buttonName={'Add room'} class="ml-auto" />
+    </div>
     
-    <InfiniteTable />
+    <InfiniteTable importbaseUrl={'rooms'} visibleFields={['name', 'registered', 'updated']} />
       
 </Layout>
