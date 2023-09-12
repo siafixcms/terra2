b2b234147b0145e889bd482b9dff16820edf5de4
root <root@cds4.localdomain>
2023-09-12 15:27:31 +0300

    a

--- src/components/PopupForm.svelte
+++ src/components/PopupForm.svelte
@@ -49,13 +49,7 @@
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  import(`./FormLayouts/${capitalizeFirstLetter(importbaseUrl.toLowerCase())}${action ? capitalizeFirstLetter(action.toLowerCase()) : ''}.svelte`).then(module => {
-    formStore.set({
-      layout: module.default,
-      handleSubmit: module.handleSubmit || null,  // Use the handler from the imported component
-      defaultData: module.defaultData || {}
-    });
-  });
+  
 </script>
 
 <button class="button" on:click={() => showPopup.set(true)}>{buttonName}</button>
