121800b2d7d4a7753e7741e09d54b5e3a359592b
root <root@cds4.localdomain>
2023-09-14 17:57:06 +0300

    a

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -6,7 +6,6 @@
 <Layout>
     <h2 class="text-2xl font-semibold text-gray-800 my-4">Users</h2>
     
-    <InfiniteTable importbaseUrl={'users'} />
     <InfiniteTable importbaseUrl={'users'} visibleFields={['login', 'name', 'surname']} />
       
 </Layout>
