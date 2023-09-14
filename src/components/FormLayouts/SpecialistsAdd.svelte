1127879cdbecd38d83956672392267ff117d36e9
root <root@cds4.localdomain>
2023-09-14 16:44:13 +0300

    a

---
+++ src/components/FormLayouts/SpecialistsAdd.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { reinitialize } from "../ReinitComponents.js";
+    import { create, setBaseUrl } from '../API.js';
+    import handleSubmitStore from '../../stores/handleSubmitStore';
+
+    export let data;
+    export let importbaseUrl;
+
+    setBaseUrl(importbaseUrl);
+    let uniqueId = importbaseUrl + '_table';
+
+    export async function handleSubmit() {
+        await create(data);
+        reinitialize.update(state => {
+            state[uniqueId] = true;
+            return state;
+        });
+    };
+
+    handleSubmitStore.set(handleSubmit);
+
+    export const defaultData = {
+        name: "",
+        type: "person",
+        jur_address: "",
+        fact_address: "",
+        email: "",
+        reg_num: "",
+        phone: "",
+        web: ""
+    };
+</script>
+
+<input bind:value={data.name} type="text" placeholder="Specialist name" required />
+
+<style>
+    input, select {
+        margin: 10px;
+    }
+</style>
