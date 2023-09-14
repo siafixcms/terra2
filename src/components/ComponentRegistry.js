63e6e5e8de9d57fb689a23736537be9a173e0e4a
root <root@cds4.localdomain>
2023-09-14 15:10:17 +0300

    a

---
+++ src/components/ComponentRegistry.js
@@ -0,0 +1,9 @@
+import ClientsAdd from './FormLayouts/ClientsAdd.svelte';
+import RoomsAdd from './FormLayouts/RoomsAdd.svelte';
+
+const componentRegistry = {
+  'ClientsAdd': ClientsAdd,
+  'RoomsAdd': RoomsAdd,
+};
+
+export default componentRegistry;
