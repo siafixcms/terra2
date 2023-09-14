07cf4fc6e700b9b908b130075f7b38eaf82320df
root <root@cds4.localdomain>
2023-09-14 16:47:05 +0300

    a

--- src/components/ComponentRegistry.js
+++ src/components/ComponentRegistry.js
@@ -1,9 +1,11 @@
 import ClientsAdd from './FormLayouts/ClientsAdd.svelte';
 import RoomsAdd from './FormLayouts/RoomsAdd.svelte';
+import SpecialistsAdd from './FormLayouts/SpecialistsAdd.svelte';
 
 const componentRegistry = {
   'ClientsAdd': ClientsAdd,
   'RoomsAdd': RoomsAdd,
+  'SpecialistsAdd': SpecialistsAdd,
 };
 
 export default componentRegistry;
