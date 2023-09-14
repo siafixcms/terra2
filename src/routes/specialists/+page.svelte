013b900770bab55b8b56a8aada950be428d27219
root <root@cds4.localdomain>
2023-09-14 16:40:50 +0300

    a

--- src/routes/specialists/+page.svelte
+++ src/routes/specialists/+page.svelte
@@ -1,11 +1,16 @@
 <script>
     import Layout from '../../components/Layout.svelte';
     import InfiniteTable from '../../components/InfiniteTable.svelte';
+    import PopupButton from '../../components/PopupForm.svelte';
 </script>
 
 <Layout>
     <h2 class="text-2xl font-semibold text-gray-800 my-4">Specialists</h2>
+
+    <div class="flex justify-end mb-4">
+        <PopupButton importbaseUrl={'specialists'} action={'add'} title={'Add a new specialist'} buttonName={'Add specialist'} class="ml-auto" />
+    </div>
     
-    <InfiniteTable />
+    <InfiniteTable importbaseUrl={'specialists'} visibleFields={['name', 'created', 'updated']} />
       
 </Layout>
