dcc6f577cdaf4244b7b2cf6ff6c26c16be4f6c3d
root <root@cds4.localdomain>
2023-09-14 15:47:46 +0300

    a

--- src/components/FormLayouts/RoomsAdd.svelte
+++ src/components/FormLayouts/RoomsAdd.svelte
@@ -1,12 +1,43 @@
 <script>
-    export let handleSubmit;
+    import { reinitialize } from "../ReinitComponents.js";
+    import { create, setBaseUrl } from '../API.js';
+    import handleSubmitStore from '../../stores/handleSubmitStore';
 
-    let data = {
-        // Your form data
+    export let data;
+    export let importbaseUrl;
+
+    setBaseUrl(importbaseUrl);
+    let uniqueId = importbaseUrl + '_table';
+
+    export async function handleSubmit() {
+        console.log('HandleSubmit triggered');
+        await create(data);
+        console.log('create called');
+        reinitialize.update(state => {
+            state[uniqueId] = true;
+            return state;
+        });
+        console.log('reinit should have happened');
     };
 
-    const submitForm = () => {
-        handleSubmit(data);
+    handleSubmitStore.set(handleSubmit);
+
+    export const defaultData = {
+        name: "",
+        type: "person",
+        jur_address: "",
+        fact_address: "",
+        email: "",
+        reg_num: "",
+        phone: "",
+        web: ""
     };
 </script>
-  
+
+<input bind:value={data.name} type="text" placeholder="Client full name" required />
+
+<style>
+    input, select {
+        margin: 10px;
+    }
+</style>
