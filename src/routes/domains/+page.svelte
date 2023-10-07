7a43fc787f6ec036b49d5855ab73e64b8b784de7
root <root@cds4.localdomain>
2023-10-07 12:52:51 +0300

    a

--- src/routes/domains/+page.svelte
+++ src/routes/domains/+page.svelte
@@ -8,6 +8,6 @@
     
     <h2 class="text-2xl font-semibold text-gray-800 my-4">Domains</h2>
 
-    <InfiniteTable importbaseUrl={'domains'} visibleFields={['name', 'created_on']} headers={{'created_on': 'Created'}} buttonName={'domain'}/>
+    <InfiniteTable importbaseUrl={'domains'} visibleFields={['name', 'status', 'created_on']} headers={{'created_on': 'Created'}} buttonName={'domain'}/>
     
 </Layout>
