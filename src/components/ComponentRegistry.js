1db2155c6c2ce7480bab365e011c5ded094a061a
root <root@cds4.localdomain>
2023-10-07 14:09:53 +0300

    a

--- src/components/ComponentRegistry.js
+++ src/components/ComponentRegistry.js
@@ -2,12 +2,14 @@ import ClientsAdd from './FormLayouts/ClientsAdd.svelte';
 import RoomsAdd from './FormLayouts/RoomsAdd.svelte';
 import SpecialistsAdd from './FormLayouts/SpecialistsAdd.svelte';
 import UsersAdd from './FormLayouts/UsersAdd.svelte';
+import DomainsAdd from './FormLayouts/DomainsAdd.svelte';
 
 const componentRegistry = {
   'ClientsAdd': ClientsAdd,
   'RoomsAdd': RoomsAdd,
   'SpecialistsAdd': SpecialistsAdd,
   'UsersAdd': UsersAdd,
+  'DomainsAdd': DomainsAdd,
 };
 
 export default componentRegistry;
