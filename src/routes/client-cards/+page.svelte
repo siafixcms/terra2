b89aae8d63aa8d8c0a6fad563b0bc56fe16061ff
root <root@cds4.localdomain>
2023-10-16 11:18:42 +0300

    a

---
+++ src/routes/client-cards/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Layout from '../../components/Layout.svelte';
+    import InfiniteTable from '../../components/InfiniteTable.svelte';
+</script>
+
+<Layout>
+    <h2 class="text-2xl font-semibold text-gray-800 my-4">Client cards</h2>  
+    
+</Layout>
